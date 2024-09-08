2e9ecc237336c2be835bab3d8b7de3aea4e5fc33
admin <andreevairinanikolaevna@gmail.com>
2024-09-08 13:23:29 +0300

    добавлен комбобокс для тегов. Поле ввода удалено

--- Cattaas.py
+++ Cattaas.py
@@ -1,7 +1,12 @@
 from tkinter import *
+from tkinter import ttk# набор улучшенных виджетов
 from PIL import Image, ImageTk # т. к. работаем с изображениями
 import requests
 from io import BytesIO # позволяет работать с 1 и 0
+
+# Список доступных тегов (спящий. прыгающий, дерущийся, черный,белый, бенгальский, сиамский, милый)
+ALLOWED_TAGS = ['sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal']
+
 
 def load_image(url):
     try:# обработка исключений
@@ -28,7 +33,7 @@ def load_image(url):
         return None # если все хорошо, вернет изображение, если плохо ничего не #вернет
 
 def open_new_window():# открыть новое окно
-    tag = tag_entry.get()# то что ввел пользователь
+    tag = tag_combobox.get()
     # самый простой способ склейки f строки. Если поле ввода пустое то
     # 'https://cataas.com/cat' (без последнего слеша)
     url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
@@ -60,24 +65,6 @@ window.title("Cats!")# назвавние
 window.geometry("600x520")# размер
 
 
-# Поле ввода для тегов
-tag_entry = Entry()# имя нужно дать обязательно иначе не смножем считать
-tag_entry.pack()
-
-# Кнопка для загрузки изображения с тегом (с текстом)
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
-
-
-
-
-
-# Добавляем кнопку для обновления изображения, после добавления меню
-# она будет не нужна, закомментируем
-# update_button = Button(text="Обновить", command=set_image)
-# command=set_image- вызывает load_image и установливает изображение в метку
-# update_button.pack() # update-обновление
-
 # Создаем меню
 menu_bar = Menu(window)# создаем меню в окне window
 window.config(menu=menu_bar)
@@ -91,9 +78,20 @@ file_menu.add_command(label="Выход", command=exit)
 
 
 
-
 url = 'https://cataas.com/cat'# адрес в интернете, по которому будем искать
 # Вызываем функцию для установки изображения в метку
+
+
+# Метка "Выбери тег"
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=ALLOWED_TAGS)# values=ALLOWED_TAGS- разрешонные значения
+tag_combobox.pack()
+
+# Кнопка для загрузки изображения с тегом (с текстом)
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 
 window.mainloop()
